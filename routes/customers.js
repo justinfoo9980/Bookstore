c7c51acc406dc9644356ebd553a9ae1fea576b19
jasonchanwq <jasonchanwq@gmail.com>
2024-03-26 11:00:05 +0800

    add customers /me

--- routes/customers.js
+++ routes/customers.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const { Customer, validate } = require('../models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -29,6 +30,15 @@ router.put('/:id', async (req, res) => {
     if (error)
         return res.status(400).send(error.details[0].message);
 
+    //const updateFields = {};
+    //if (req.body.name) updateFields.name = req.body.name;
+    //if (req.body.defaultShippingAddress) updateFields.defaultShippingAddress = req.body.defaultShippingAddress;
+    //if (req.body.defaultBillingAddress) updateFields.defaultBillingAddress = req.body.defaultBillingAddress;
+
+    //const customer = await Customer.findByIdAndUpdate(req.params.id,
+    //    updateFields,
+    //    { new: true });
+
     const customer = await Customer.findByIdAndUpdate(req.params.id,
         {
             name: req.body.name,
@@ -51,6 +61,18 @@ router.delete('/:id', async (req, res) => {
     res.send(customer);
 });
 
+router.get('/me', auth, async (req, res) => {
+    try {
+        const customer = await Customer.findOne({ userId: req.user._id });
+        if (!customer) return res.status(404).send('Customer not found');
+        res.send(customer);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+//anything with :id must be placed last  else it will confuse to think that the string is the :id
 router.get('/:id', async (req, res) => {
     const customer = await Customer.findById(req.params.id);
 
